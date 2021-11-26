1f0b04dc8ba371aa5817b3cca862fd1190d4ea7c
Nong Minh Hieu <hieubkvn123@gmail.com>
2021-11-26 14:54:02 +0800

    Added data and modified the mAP_curve script

--- mAP_curve.py
+++ mAP_curve.py
@@ -23,6 +23,7 @@ COLORS = [
 parser = ArgumentParser()
 parser.add_argument('--gt', required=True, type=str, help='Path to ground truth bounding boxes json file')
 parser.add_argument('--pr', required=True, type=str, help='Path to predicted bounding boxes json file')
+parser.add_argument('--iou', required=False, type=float, default=0.6, help='IOU threshold')
 args = vars(parser.parse_args())
 
 def calc_iou_individual(pred_box, gt_box):
@@ -259,10 +260,9 @@ def plot_pr_curve(
 
     if color is None:
         color = COLORS[0]
-    ax.scatter(recalls, precisions, label=label, s=20, color=color)
+    ax.plot(recalls, precisions, label=label, color=color, linestyle='--')
     ax.set_xlabel('recall')
     ax.set_ylabel('precision')
-    ax.set_title('Precision-Recall curve for {}'.format(category))
     ax.set_xlim([0.0,1.3])
     ax.set_ylim([0.0,1.2])
     return ax
@@ -275,19 +275,23 @@ if __name__ == "__main__":
     with open(args['pr']) as infile:
         pred_boxes = json.load(infile)
 
-    # Runs it for one IoU threshold
-    iou_thr = 0.7
-    start_time = time.time()
-    data = get_avg_precision_at_iou(gt_boxes, pred_boxes, iou_thr=iou_thr)
-    end_time = time.time()
-    print('Single IoU calculation took {:.4f} secs'.format(end_time - start_time))
-    print('avg precision: {:.4f}'.format(data['avg_prec']))
+    # Precision and recall for the pre-defined IOU threshold
+    data = get_avg_precision_at_iou(gt_boxes, pred_boxes, iou_thr=args['iou'])
+    precisions = data['precisions']
+    recalls = data['recalls']
+    ax = plot_pr_curve(precisions, recalls, label='IOU={:.2f}'.format(args['iou']))
 
+    for xval in np.linspace(0.0, 1.0, 11):
+        plt.vlines(xval, 0.0, 1.1, color='gray', alpha=0.3, linestyles='dashed')
+    plt.legend()
+    plt.show()
+
+    # Precision and recall for different IOU thresholds
     start_time = time.time()
     ax = None
     avg_precs = []
     iou_thrs = []
-    for idx, iou_thr in enumerate(np.linspace(0.5, 0.95, 10)):
+    for idx, iou_thr in enumerate(np.linspace(0.6, 0.95, 10)):
         data = get_avg_precision_at_iou(gt_boxes, pred_boxes, iou_thr=iou_thr)
         avg_precs.append(data['avg_prec'])
         iou_thrs.append(iou_thr)
@@ -303,9 +307,9 @@ if __name__ == "__main__":
     print('map: {:.2f}'.format(100*np.mean(avg_precs)))
     print('avg precs: ', avg_precs)
     print('iou_thrs:  ', iou_thrs)
-    # plt.legend(loc='upper right', title='IOU Thr', frameon=True)
     for xval in np.linspace(0.0, 1.0, 11):
         plt.vlines(xval, 0.0, 1.1, color='gray', alpha=0.3, linestyles='dashed')
     end_time = time.time()
     print('\nPlotting and calculating mAP takes {:.4f} secs'.format(end_time - start_time))
+    plt.legend()
     plt.show()
