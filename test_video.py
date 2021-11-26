6c0e81731574922bf71faa5a42f5a54422858c61
hieubkvn123 <hieubkvn123@gmail.com>
2021-11-26 14:31:00 +0700

    Fixed the test_video.py file

--- test_video.py
+++ test_video.py
@@ -1,4 +1,5 @@
 import cv2
+import imageio
 import tqdm
 import sys, traceback
 import time
@@ -11,7 +12,8 @@ from imutils.video import WebcamVideoStream
 parser = ArgumentParser()
 parser.add_argument('--input', required=True, help='Path to input test video')
 parser.add_argument('--model', required=False, default='center', help='Detection model used')
-parser.add_argument('--output', required=False, default='videos/output.avi', help='Path to output video')
+parser.add_argument('--output', required=False, default='videos/output.gif', help='Path to output video')
+parser.add_argument('--display', required=False, type=bool, default=False, help='Wether to display predicted video or not')
 args = vars(parser.parse_args())
 
 # List of models for detection
@@ -41,32 +43,54 @@ time.sleep(2.0)
 
 # Start the loop
 print('[INFO] Running frame-by-frame prediction and writing result ... ')
-while(True):
-    try:
-        # 1. Load frames from the camera
-        ret, frame = in_stream.read()
-        if(not ret): 
+
+frames = []
+with tqdm.tqdm(total=None) as pbar:
+    while(True):
+        try:
+            # 1. Load frames from the camera
+            ret, frame = in_stream.read()
+            if(not ret): 
+                break
+
+            # 2. Image preprocessing
+            frame = mx.nd.array(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).astype('uint8')
+            rgb_nd, frame = preprocess_func(frame, short=512, max_size=700)
+            
+            # 3. Run inference on current frame
+            class_IDS, scores, bounding_boxes = net(rgb_nd)
+
+            # 4. Plot the bounding boxes
+            frame = utils.viz.cv_plot_bbox(frame, bounding_boxes[0], scores[0], class_IDS[0], class_names=net.classes)
+
+            if(args['display']):
+                # 5. Plot the frame with the bounding boxes
+                utils.viz.cv_plot_image(frame)
+
+            # 6. Store the result for writing
+            frames.append(frame)
+
+            key = cv2.waitKey(1)
+            if(key == ord('q')): 
+                break
+
+            pbar.update(1)
+        except KeyboardInterrupt:
+            print('[INFO] Key board interrupted')
+            break
+        except:
+            traceback.print_exc(file=sys.stdout)
             break
 
-        # 2. Image preprocessing
-        frame = mx.nd.array(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).astype('uint8')
-        rgb_nd, frame = preprocess_func(frame, short=512, max_size=700)
-        
-        # 3. Run inference on current frame
-        class_IDS, scores, bounding_boxes = net(rgb_nd)
+# Write the result into a gif
+print(f'[INFO] Writing results to gif file {args["output"]}')
+images = []
+with tqdm.tqdm(total=len(frames)) as pbar:
+    for frame in frames:
+        images.append(imageio.core.util.Array(frame))
+        pbar.update(1)
 
-        # 4. Plot the bounding boxes
-        frame = utils.viz.cv_plot_bbox(frame, bounding_boxes[0], scores[0], class_IDS[0], class_names=net.classes)
-
-        # 5. Plot the frame with the bounding boxes
-        utils.viz.cv_plot_image(frame)
-
-        key = cv2.waitKey(1)
-        if(key == ord('q')): 
-            break
-    except:
-        traceback.print_exc(file=sys.stdout)
-        break
+imageio.mimsave(args['output'], images, fps=10)
 
 in_stream.release()
 cv2.destroyAllWindows()
